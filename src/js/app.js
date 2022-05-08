09b353d62236a17486e9fc5300cc18b7aec99e26
Irina Paukova <63339174+Irina-Paukova@users.noreply.github.com>
2022-05-08 08:40:48 +0300

    remove //

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,3 @@
-// TODO: write your code here
-
 function colorIndication(character) {
   if (character.health > 50) {
     return 'healthy';
@@ -8,13 +6,5 @@ function colorIndication(character) {
   }
   return 'wounded';
 }
-
-// function colorIndication(character) {
-//   const { name, health } = character;
-
-//   alert(name);
-
-//   return (health > 50 && 'healthy') || (health <= 50 && health >= 15 && 'wounded') || 'critical';
-// }
 
 export default colorIndication;
